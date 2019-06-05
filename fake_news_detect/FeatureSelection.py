7986c931c41ef67d75109112257b3f99d5163715
Uladzislau Vasilyuk <vlad007vladpedro@gmail.com>
2019-06-06 00:58:40 +0300

    added fake news detection

---
+++ fake_news_detect/FeatureSelection.py
@@ -0,0 +1,98 @@
+import DataPrep
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk.corpus
+
+# bag of words technique
+# feature vector - document term matrix
+countV = CountVectorizer()
+train_count = countV.fit_transform(DataPrep.train_news['Statement'].values)
+
+print(countV)
+print(train_count)
+
+
+# print training doc term matrix
+def get_countVectorizer_stats():
+    # vocab size
+    train_count.shape
+
+    # check vocabulary]
+    print(countV.vocabulary_)
+
+    print(countV.get_feature_names()[:25])
+
+
+# tf-idf
+tfidfV = TfidfTransformer()
+train_tfidf = tfidfV.fit_transform(train_count)
+
+
+def get_tfidf_stats():
+    train_tfidf.shape
+    # get train data feature names
+    print(train_tfidf.A[:10])
+
+
+tfidf_ngram = TfidfVectorizer(stop_words='english', ngram_range=(1, 4), use_idf=True, smooth_idf=True)
+
+# POS Tagging
+tagged_sentences = nltk.corpus.treebank.tagged_sents()
+
+cutoff = int(.75 * len(tagged_sentences))
+training_sentences = DataPrep.train_news['Statement']
+
+print(training_sentences)
+
+
+# training POS tagger based on words
+def features(sentence, index):
+    """ sentence: [w1, w2, ...], index: the index of the word """
+    return {
+        'word': sentence[index],
+        'is_first': index == 0,
+        'is_last': index == len(sentence) - 1,
+        'is_capitalized': sentence[index][0].upper() == sentence[index][0],
+        'is_all_caps': sentence[index].upper() == sentence[index],
+        'is_all_lower': sentence[index].lower() == sentence[index],
+        'prefix-1': sentence[index][0],
+        'prefix-2': sentence[index][:2],
+        'prefix-3': sentence[index][:3],
+        'suffix-1': sentence[index][-1],
+        'suffix-2': sentence[index][-2:],
+        'suffix-3': sentence[index][-3:],
+        'prev_word': '' if index == 0 else sentence[index - 1],
+        'next_word': '' if index == len(sentence) - 1 else sentence[index + 1],
+        'has_hyphen': '-' in sentence[index],
+        'is_numeric': sentence[index].isdigit(),
+        'capitals_inside': sentence[index][1:].lower() != sentence[index][1:]
+    }
+
+
+def untag(tagged_sentence):
+    return [w for w, t in tagged_sentence]
+
+
+with open("glove.6B.50d.txt", "rb") as lines:
+    w2v = {line.split()[0]: np.array(map(float, line.split()[1:]))
+           for line in lines}
+
+
+
+
+class MeanEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        self.dim = len(word2vec.itervalues().next())
+
+    def fit(self, X, y):
+        return self
+
+    def transform(self, X):
+        return np.array([
+            np.mean([self.word2vec[w] for w in words if w in self.word2vec]
+                    or [np.zeros(self.dim)], axis=0)
+            for words in X
+        ])
--- news/tasks.py
+++ news/tasks.py
@@ -1,24 +1,28 @@
-# from mysite.celery import app
-# import json
-# from celery.task.schedules import crontab
-# from celery.decorators import periodic_task
-# from newsapi import NewsApiClient
-# from news.models import TopHeadline
-#
-# news = NewsApiClient(api_key='959ed0e9826742709ee20cbc5632d5d5');
-#
-#
-# @periodic_task(run_every=(crontab(), name="get_top_news", ignore_result=True)
-# def get_top_news():
-#     top_headlines_source = news.get_top_headlines(sources='google-news')
-#     encoded_headlines = json.dumps(top_headlines_source, sort_keys=True, indent=4)
-#     decoded_headlines = json.loads(encoded_headlines).get('articles')
-#     for i in range(10):
-#         temp = decoded_headlines[i]
-#         headline = TopHeadline(author=temp.get('author'), title=temp.get('title'), description=temp.get('description'),
-#                                url=temp.get('url'), urlToImage=temp.get('urlToImage'),
-#                                publishedAt=temp.get('publishedAt'))
-#
-#         if not TopHeadline.objects.filter(title=headline.title):
-#             headline.save()
-#     print("iteration")
+from mysite.celery import app
+import json
+from fake_news_detect.prediction import detecting_fake_news
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
+from newsapi import NewsApiClient
+from news.models import TopHeadline
+
+news = NewsApiClient(api_key='959ed0e9826742709ee20cbc5632d5d5');
+
+
+@periodic_task(run_every=crontab(), name="get_top_news", ignore_result=True)
+def get_top_news():
+    top_headlines_source = news.get_top_headlines(sources='google-news')
+    encoded_headlines = json.dumps(top_headlines_source, sort_keys=True, indent=4)
+    decoded_headlines = json.loads(encoded_headlines).get('articles')
+    for i in range(10):
+        temp = decoded_headlines[i]
+        headline = TopHeadline(author=temp.get('author'), title=temp.get('title'), description=temp.get('description'),
+                               url=temp.get('url'), urlToImage=temp.get('urlToImage'),
+                               publishedAt=temp.get('publishedAt'))
+        res = detecting_fake_news(headline.description)
+        headline.is_true = res[0]
+        headline.is_true_prob = res[1]
+
+        if not TopHeadline.objects.filter(title=headline.title):
+            headline.save()
+    print("iteration")
