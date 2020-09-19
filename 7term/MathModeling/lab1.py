26b2a2722a6b4f8c5c6c7c37f2c7ea9499614995
Uladzislau <devuladblr@gmail.com>
2020-09-19 19:34:20 +0300

    fixed, final version

--- 7term/MathModeling/lab1.py
+++ 7term/MathModeling/lab1.py
@@ -5,23 +5,23 @@ import matplotlib.pyplot as plt
 class Solution:
     def get_mid_square_random_values(self, a0: int, n: int) -> List[float]:
         seq = []
-        temp = ''.join([val for val in str(a0 ** 2)])
+        temp = str(a0 ** 2)
         j = 1
-        while j < 5 and int(temp[j:j + 4]) < 1000:
+        while j < len(temp) - 3 and int(temp[j:j + 4]) < 100:
             j += 1
         a1 = int(temp[j:j + 4])
-        if a1 < 1000:
+        if a1 < 100:
             print('Error. Bad a0. Choose another value')
             return []
         seq.append(a1)
 
         for i in range(1, n + 1):
-            temp = ''.join([val for val in str(seq[i - 1] ** 2)])
+            temp = str(seq[i - 1] ** 2)
             j = 1
-            while j < 5 and int(temp[j:j + 4]) < 1000:
+            while j < len(temp) - 1 and int(temp[j:j + 4]) < 100:
                 j += 1
             new_a = int(temp[j:j + 4])
-            if new_a < 1000:
+            if new_a < 100:
                 print('Error. Algorithm was interrupted. Choose another start value')
                 return seq
             seq.append(new_a)
@@ -58,13 +58,13 @@ class Solution:
             n10000[int(elem * k)] += 1
             expected_value += elem
         n10000[k - 1] += n10000.pop()
-        p10000 = [elem / 10000 for elem in n10000]
+        p10000 = [elem / 10000000 for elem in n10000]
 
-        expected_value /= 10000
+        expected_value /= 10000000
         expected_value_pow_2 = expected_value ** 2
         for elem in arr10000:
             dispersion += elem ** 2 - expected_value_pow_2
-        dispersion /= 10000
+        dispersion /= 10000000
 
         return expected_value, dispersion, p100, p10000
 
@@ -95,24 +95,28 @@ if __name__ == '__main__':
     solution = Solution()
 
     arr_square_100 = solution.get_mid_square_random_values(1234, 100)
-    arr_square_10000 = solution.get_mid_square_random_values(4823, 10000)
+    arr_square_10000 = solution.get_mid_square_random_values(4823, 10000000)
 
     expected_value, dispersion, p100, p10000 = solution.uniformity_test(arr_square_100, arr_square_10000, 10)
     solution.draw_plots(p100, p10000)
     print('expected_value', expected_value, 'dispersion', dispersion)
-    new_arr = solution.get_mid_square_random_values(3422, 10000)
-    m_y, d_y = solution.get_expected_value_and_dispersion(new_arr)
-    m_xy, _ = solution.get_expected_value_and_dispersion([arr_square_10000[i] * new_arr[i] for i in range(10000)])
+
+    s = 20
+
+    arr1, arr2 = arr_square_10000[:len(arr_square_10000) - s], arr_square_10000[s:]
+    m_x, d_x = solution.get_expected_value_and_dispersion(arr1)
+    m_y, d_y = solution.get_expected_value_and_dispersion(arr2)
+    m_xy, _ = solution.get_expected_value_and_dispersion([arr1[i] * arr2[i] for i in range(len(arr1))])
     print('Coefficient  correlation', solution.independence_test(expected_value, dispersion, m_y, d_y, m_xy))
 
-
-    arr_mul_100 = solution.get_multiplicative_congruential_random_values(42949, 72315, 124, 100)
-    arr_mul_10000 = solution.get_multiplicative_congruential_random_values(42949, 72315, 124, 10000)
+    arr_mul_100 = solution.get_multiplicative_congruential_random_values(68719476736, 23534635, 1224, 100)
+    arr_mul_10000 = solution.get_multiplicative_congruential_random_values(68719476736, 23534635, 1224, 10000000)
 
     expected_value, dispersion, p100, p10000 = solution.uniformity_test(arr_mul_100, arr_mul_10000, 10)
     solution.draw_plots(p100, p10000)
     print('expected_value', expected_value, 'dispersion', dispersion)
-    new_arr = solution.get_multiplicative_congruential_random_values(4294913, 36315, 124, 10000)
-    m_y, d_y = solution.get_expected_value_and_dispersion(new_arr)
-    m_xy, _ = solution.get_expected_value_and_dispersion([arr_square_10000[i] * new_arr[i] for i in range(10000)])
+    arr1, arr2 = arr_mul_10000[:len(arr_mul_10000) - s], arr_mul_10000[s:]
+    m_x, d_x = solution.get_expected_value_and_dispersion(arr1)
+    m_y, d_y = solution.get_expected_value_and_dispersion(arr2)
+    m_xy, _ = solution.get_expected_value_and_dispersion([arr1[i] * arr2[i] for i in range(len(arr1))])
     print('Coefficient  correlation', solution.independence_test(expected_value, dispersion, m_y, d_y, m_xy))
